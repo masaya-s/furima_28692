afac6b5c32eed2b4a7e5549764595e336e15a66e
masaya-s <masaya.shoji000@gmail.com>
2020-09-09 19:38:23 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,8 +33,12 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    binding.pry
+    if item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
